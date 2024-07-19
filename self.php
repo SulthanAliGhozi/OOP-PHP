95214ed1830070580836c3f4faa1cc6de2523146
SulthanAliGhozi <s.a.ghozi@gmail.com>
2024-07-19 14:31:54 +0700

    update file self.php

---
+++ self.php
@@ -0,0 +1,7 @@
+<?php
+/*sama sepertu keyword $this, keyword self juga
+memiliki pengertian dan fungsi yang sama. Yang 
+membedakan dengan keyword $this adalah bahwa keyword self 
+hanya untuk memanggil property atau methide yang bersifat
+statis dan juga untuk memanggil konstanta.*/
+?>
